6c11ee0becf65d5e9b380c259862257f9acc2626
Erick Silva Villalta <>
2024-08-24 20:22:02 -0500

    Ejercicio pow

---
+++ src/ejercicio/de/pow/EjercicioDePow.java
@@ -0,0 +1,15 @@
+
+package ejercicio.de.pow;
+
+import java.util.Scanner;
+        
+public class EjercicioDePow {
+
+    public static void main(String[] args) {
+        
+        double pow = Math.pow(5, 2);
+                
+        System.out.println(pow);
+    }
+    
+}
